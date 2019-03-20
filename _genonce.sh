ff65fe6f083321045e37255d305aa8e98f514caf
Dave Pyke <david.pyke@readycomputing.com>
2019-03-19 22:49:55 -0400

    Changes as per Graham

--- _genonce.sh
+++ _genonce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-java -jar framework/ant/ant-launcher.jar -buildfile framework/build.xml -DupdatePublisher=N
+time java -jar framework/ant/ant-launcher.jar -buildfile framework/build.xml -DupdatePublisher=N
